6c06b589f6c15b145e3798535d6bfa9eb10743fe
Destefanii <matheus.destefani@etec.sp.gov.br>
2021-06-04 18:35:26 -0300

    boto3

--- auto/github_actions/deploy.py
+++ auto/github_actions/deploy.py
@@ -1,5 +1,6 @@
 import boto3
 import logging
+from botocore.exceptions import ClientError
 import os
 
 logging.getLogger().setLevel(logging.INFO)
@@ -22,6 +23,26 @@ def create_stack(stack_name, template_body, **kwargs):
     cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
     logging.info(f'CREATE COMPLETE')
 
+def update_stack(stack_name, template_body, **kwargs):
+    try:
+        cloudformation_client.update_stack(
+            StackName=stack_name,
+            Capabilities=['CAPABILITY_IAM','CAPABILITY_NAMED_IAM'],
+            TemplateBody = template_body
+        )
+
+    except ClientError as e:
+        if 'No updates are to be performed' in str(e):
+            logging.info(f'SKIPPING UPDATE: No updates to be performed at stack {stack_name}')
+            return e
+
+    cloudformation_client.get_waiter('stack_update_complete').wait(
+        StackName=stack_name, 
+        WaiterConfig={'Delay': 5, 'MaxAttempts': 600}
+    )
+
+    cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
+    logging.info(f'CREATE COMPLETE')
 
 def get_existing_stacks():
     response = cloudformation_client.list_stacks(
@@ -40,13 +61,13 @@ def get_existing_stacks():
                             'DELETE_FAILED', 
                             'IMPORT_COMPLETE', 
                             'DELETE_IN_PROGRESS', 
+                            'ROLLBACK_COMPLETE', 
                             'ROLLBACK_FAILED', 
                             'IMPORT_ROLLBACK_COMPLETE', 
                             'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS', 
                             'CREATE_COMPLETE', 
                             'IMPORT_ROLLBACK_FAILED', 
-                            'UPDATE_ROLLBACK_COMPLETE',
-                            'ROLLBACK_COMPLETE']
+                            'UPDATE_ROLLBACK_COMPLETE']
     )
 
     return [stack['StackName'] for stack in response ['StackSummaries']]
@@ -61,8 +82,12 @@ def create_or_update_stack():
 
     existing_stacks = get_existing_stacks()
 
-    logging.info(f'CREATING STACK {stack_name}')
-    create_stack(stack_name, template_body)
+    if stack_name in existing_stacks:
+        logging.info(f'UPDATING STACK {stack_name}')
+        update_stack(stack_name, template_body)
+    else:
+        logging.info(f'CREATING STACK {stack_name}')
+        update_stack(stack_name, template_body)
 
 if __name__ == '__main__':
     create_or_update_stack()
