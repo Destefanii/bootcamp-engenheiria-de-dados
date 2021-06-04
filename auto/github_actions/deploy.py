e7cc955e700cbe3e4d27798b31195204c0fe83c4
Destefanii <matheus.destefani@etec.sp.gov.br>
2021-06-04 18:22:22 -0300

    boto3

--- auto/github_actions/deploy.py
+++ auto/github_actions/deploy.py
@@ -1,6 +1,5 @@
 import boto3
 import logging
-from botocore.exceptions import ClientError
 import os
 
 logging.getLogger().setLevel(logging.INFO)
@@ -23,26 +22,6 @@ def create_stack(stack_name, template_body, **kwargs):
     cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
     logging.info(f'CREATE COMPLETE')
 
-def update_stack(stack_name, template_body, **kwargs):
-    try:
-        cloudformation_client.update_stack(
-            StackName=stack_name,
-            Capabilities=['CAPABILITY_IAM','CAPABILITY_NAMED_IAM'],
-            TemplateBody = template_body
-        )
-
-    except ClientError as e:
-        if 'No updates are to be performed' in str(e):
-            logging.info(f'SKIPPING UPDATE: No updates to be performed at stack {stack_name}')
-            return e
-
-    cloudformation_client.get_waiter('stack_update_complete').wait(
-        StackName=stack_name, 
-        WaiterConfig={'Delay': 5, 'MaxAttempts': 600}
-    )
-
-    cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
-    logging.info(f'CREATE COMPLETE')
 
 def get_existing_stacks():
     response = cloudformation_client.list_stacks(
